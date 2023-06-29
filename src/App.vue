7a793db3101af1ea7df90de0a120a756064eea99
zhaseo1 <30850614+zhaseo1@users.noreply.github.com>
2023-06-29 22:05:53 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,26 +14,20 @@ button {
 }
 </style>
 <script setup>
-new OpenInstall({
-  /*appKey必选参数，平台为每个应用分配的ID*/
-  appKey: "dpz2bf",
-  onready: function () {
+// new OpenInstall({
+//   /*appKey必选参数，平台为每个应用分配的ID*/
+//   appKey: "dpz2bf",
+//   onready: function () {
     let downloadButton = document.querySelector(".downloadButton");
     downloadButton.addEventListener("click", function () {
-      const isiOS = navigator.userAgent.match('iPad') || navigator.userAgent.match('iPhone') || navigator.userAgent.match('iPod');
-      const isAndroid = navigator.userAgent.match('Android');
-
-      let fallbackLink = "https://metatravers.oss-cn-hongkong.aliyuncs.com/app-release.apk";
-      // Mobile
-      if (isiOS || isAndroid) {
-        document.getElementById('loader').src = "dpz2bf://";
-        fallbackLink = isAndroid ? "https://metatravers.oss-cn-hongkong.aliyuncs.com/app-release.apk" :
+      let fallbackLink = "https://down.ttiossign.com/#/join/6qWlf5";
+      document.getElementById('loader').src = "dpz2bf://";
+      fallbackLink = getOS() === "android" ? "https://metatravers.oss-cn-hongkong.aliyuncs.com/app-release.apk" :
           "https://down.ttiossign.com/#/join/6qWlf5";
-      }
       window.setTimeout(function () { window.location.replace(fallbackLink); }, 1);
     });
-  }
-});
+//   }
+// });
 
 function getOS() {
   const isiOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
